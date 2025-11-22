0043e593798c0821360c1639eed32aecefc229a1
daniel-crvlh <rainhodaniel22@gmail.com>
2025-11-22 22:46:08 +0100

    Add image

--- src/main/kotlin/org/jetbrains/plugins/template/MyProcessListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/MyProcessListener.kt
@@ -1,15 +1,16 @@
 package org.jetbrains.plugins.template;
 
-import com.intellij.execution.ExecutionListener
 import com.intellij.execution.process.ProcessAdapter
 import com.intellij.execution.process.ProcessEvent
-import com.intellij.execution.process.ProcessHandler
 import com.intellij.openapi.util.Key
 
 class MyProcessListener : ProcessAdapter() {
 
+    val emptyStringArray = arrayOf<String>()
+
     override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
         val line = event.text
+        
         if (line.contains("Exception") || line.contains("ERROR")) {
             println("🔥 RUNTIME ERROR: $line")
         }
