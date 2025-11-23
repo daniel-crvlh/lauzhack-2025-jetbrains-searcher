7ca7cc447c3dc6d56118cfbb7375eb92302cf08d
alexandre.benzonana <alexandre.benzonana@master.hes-so.ch>
2025-11-23 03:02:51 +0100

    add short description

--- api/main.py
+++ api/main.py
@@ -16,6 +16,7 @@ class PredictRequest(BaseModel):
 class PredictResponse(BaseModel):
     code: str
     explanation : str
+    shortDescription: str
 
 @app.get("/")
 def read_root():
@@ -31,12 +32,16 @@ def predictPost(req: PredictRequest):
     messages=[{"role":"system", "content":f"You're an expert in {req.language} I need you to read the following code that is in {req.language} only answer with {req.language} code no explanation and fix the {req.function} error"},{"role": "user", "content": f"code:{req.code} --- error: {req.error} "}],
     )
 
+    shortDescriptionRes = client.chat.completions.create(
+    model="openai/gpt-oss-20b",
+    messages=[{"role":"system", "content":f"You're an expert in {req.language} given the {req.function} error and {req.language} code, explain why the following solution is the best, answer with a short answer at most 1 sentences"},{"role": "user", "content": f"code:{req.code},  error:{req.error}, solution:{completion.choices[0].message.content}"}],
+    )
     explanationRes = client.chat.completions.create(
-    model="moonshotai/Kimi-K2-Instruct-0905",
+    model="openai/gpt-oss-20b",
     messages=[{"role":"system", "content":f"You're an expert in {req.language} given the {req.function} error and {req.language} code, explain why the following solution is the best"},{"role": "user", "content": f"code:{req.code},  error:{req.error}, solution:{completion.choices[0].message.content}"}],
     )
 
-    return {"code": completion.choices[0].message.content, "explanation": explanationRes.choices[0].message.content}
+    return {"code": completion.choices[0].message.content, "explanation": explanationRes.choices[0].message.content, "shortDescription": shortDescriptionRes.choices[0].message.content}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
